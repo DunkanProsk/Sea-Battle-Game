83dbaee082ea14c8690005ecba7c1e1e7d163ffe
DunkanProsk <deeeeeeeenchik@yandex.ru>
2020-03-20 18:10:40 +0300

    add

--- scripts/script.js
+++ scripts/script.js
@@ -14,11 +14,18 @@
 
 let arrUser = [];
 let arrAi = [];
-let checkGetShip = 0;
-let table = document.getElementById('table__one');
-let ships = document.getElementById('ship__tabel');
+let checkGetShip = 1;
 
-(function pushArr(arr) {
+let table = document.getElementById('table__one');
+table.onmouseover = table.onmouseout = handler;
+
+let ships = document.getElementById('ship__tabel');
+let buttonReset = document.getElementById('buttonReset');
+let divShips = document.getElementById('ship__value').innerHTML;
+
+pushArr(arrUser);
+
+function pushArr(arr) {
   for (let i = 0; i < 10; i++){
     arr[i] = [];
     for (var j = 0; j < 10; j++) {
@@ -27,7 +34,7 @@ let ships = document.getElementById('ship__tabel');
   };
 
   return arr;
-})(arrUser);
+};
 
 function pushDelShip(x, y, arrPush, val) {
   (val == 'push') ? arrPush[x][y] = 1 : arrPush[x][y] = 0;
@@ -43,12 +50,36 @@ function render() {
           document.getElementById(`${i} - ${j}`).textContent = '1';
         } else {
           document.getElementById(`${i} - ${j}`).textContent = '0';
+          document.getElementById(`${i} - ${j}`).style.backgroundColor = '#4b77a8';
         };
       };
     };
   };
 
   createTable(arrUser);
+};
+
+function handler(event) {
+  let target = event.target;
+  let idVal = target.id.split(' - ');
+  let x = Number(idVal[0]);
+  let y = Number(idVal[1]);
+
+  if (event.type == 'mouseover') {
+    if(checkEmptyOne(x, y, arrUser)) {
+      target.style.background = '#6cf7aa';
+    } else {
+      target.style.background = '#fd344fc2';
+    };
+  };
+
+  if (event.type == 'mouseout') {
+    if(target.textContent == 0) {
+      target.style.background = ''
+    } else {
+      target.style.background = 'white';
+    };
+  };
 };
 
 table.onclick = function(event) {
@@ -58,54 +89,67 @@ table.onclick = function(event) {
   let y = Number(idVal[1]);
 
   if (target.tagName != 'TD') return;
+  
+  if(target.textContent == 0) {
+    if(checkEmptyOne(x, y, arrUser)) {
+      target.style.backgroundColor = 'white';
 
-  if(checkGetShip == 1) {
-    if(target.textContent == 0) {
-      if(checkEmptyOne(x, y, arrUser)) {
-        target.style.backgroundColor = 'white';
-
-        pushDelShip(x, y, arrUser, 'push');
-        render();
-      } else {
-        alert('Клетка не подходит!');
-      };
+      pushDelShip(x, y, arrUser, 'push');
+      render();
     } else {
-      target.style.backgroundColor = '';
-
-      pushDelShip(x, y, arrUser, 'del');
+      alert('Клетка не подходит!');
     };
   } else {
-    alert('Выберете карабль!');
+    target.style.backgroundColor = '';
+
+    pushDelShip(x, y, arrUser, 'del');
   };
 };
 
-ships.onmousedown = function(event) {
-  let value = document.getElementById(event.target.id);
+buttonReset.onclick = function(event) {
+  ships.innerHTML = '';
+  
+  ships.innerHTML = `
+    <div id="ship__flex">
+    <div class="onePal" id="shipOneOne"> </div>
+    <div class="onePal" id="shipOneTwo"> </div>
+    <div class="onePal" id="shipOneThree"> </div>
+    <div class="onePal" id="shipOneFour"> </div>
+    <div class="twoPal" id="shipTwoOne"> </div>
+    <div class="twoPal" id="shipTwoTwo"> </div>
+    <div class="twoPal" id="shipTwoThree"> </div>
+    </div>
+    <div id="ship__flex">
+      <div class="threePal" id="shipThreeOne"> </div>
+      <div class="threePal" id="shipThreeTwo"> </div>
+      <div class="fourPal" id="shipFourOne"> </div>
+    </div>
+  `;
+  
+  pushArr(arrUser);
+  render();
+};
 
-  console.log(value);
+ships.onclick = function(event) {
+  if((event.target.id == 'ship__tabel') || (event.target.id == 'ship__flex')) return;
 
-  value.style.position = 'absolute';
-  value.style.zIndex = 1000;
-    
-  document.body.append(value);
-
-  moveAt(event.pageX, event.pageY);
-
-  function moveAt(pageX, pageY) {
-    value.style.left = pageX - value.offsetWidth / 2 + 'px';
-    value.style.top = pageY - value.offsetHeight / 2 + 'px';
-  }
-
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-  }
-
-  document.addEventListener('mousemove', onMouseMove);
-
-  value.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    value.onmousedown = null;
+  if(event.target.className == 'onePal') {
+    checkGetShip = 1;
   };
+
+  if(event.target.className == 'twoPal') {
+    checkGetShip = 2;
+  };
+
+  if(event.target.className == 'threePal') {
+    checkGetShip = 3;
+  };
+
+  if(event.target.className == 'fourPal') {
+    checkGetShip = 4;
+  };
+
+  document.getElementById(event.target.id).remove();
 };
 
 function checkEmptyOne(x, y, arr) {
@@ -286,3 +330,33 @@ function checkEmpty(arr) {
 function checkEmpty(arr) {
   
 };
+
+// ships.onmousedown = function(event) {                                                //попытка drag&drop для кораблей  
+//   let value = document.getElementById(event.target.id);
+
+//   if((event.target.id == 'ship__tabel') || (event.target.id == 'ship__flex')) return;
+
+//   value.style.position = 'absolute';
+//   value.style.zIndex = 1000;
+    
+//   document.body.append(value);
+
+//   moveAt(event.pageX, event.pageY);
+
+//   function moveAt(pageX, pageY) {
+//     value.style.left = pageX - value.offsetWidth / 2 + 'px';
+//     value.style.top = pageY - value.offsetHeight / 2 + 'px';
+//   };
+
+//   function onMouseMove(event) {
+//     moveAt(event.pageX, event.pageY);
+//   };
+
+//   document.addEventListener('mousemove', onMouseMove);
+
+//   value.onclick = function() {
+//     document.removeEventListener('mousemove', onMouseMove);
+//     checkGetShip == 1;
+//     value.onmousedown = null;
+//   };
+// };
